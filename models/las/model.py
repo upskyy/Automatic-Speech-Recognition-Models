5fdbf8d08c7057c0a74fdcede5e805da74e20116
Sangchun Ha <seomk9896@naver.com>
2021-07-03 14:26:58 +0900

    Update model.py

--- models/las/model.py
+++ models/las/model.py
@@ -66,10 +66,7 @@ class ListenAttendSpell(nn.Module):
             if use_joint_ctc_attention is False, return None.
             **decoder_output_prob** (Tensor): ``(batch, seq_length, num_vocabs)``
         """
-        self.encoder.rnn.flatten_parameters()
         encoder_output, encoder_output_prob, encoder_output_lens = self.encoder(encoder_inputs, encoder_inputs_lens)
-
-        self.decoder.rnn.flatten_parameters()
         decoder_output_prob = self.decoder(decoder_inputs, encoder_output, teacher_forcing_ratio)
 
         return encoder_output_prob, encoder_output_lens, decoder_output_prob
